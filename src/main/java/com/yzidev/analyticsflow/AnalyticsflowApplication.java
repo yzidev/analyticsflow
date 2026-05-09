99755030b21aa19083a1c61c6ba1b9a185747b6e
aizacoders <azizmiftakhul19@gmail.com>
2026-05-09 22:11:12 +0700

    feat: add init files

---
+++ src/main/java/com/yzidev/analyticsflow/AnalyticsflowApplication.java
@@ -0,0 +1,13 @@
+package com.yzidev.analyticsflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzidev/analyticsflow/AnalyticsflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzidev.analyticsflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
